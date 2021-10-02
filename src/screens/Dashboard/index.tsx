80a7bcc0093692b6382d5c4387b6c1a19a33e326
thiagorasan <trs.tiba@gmail.com>
2021-10-01 22:21:45 -0300

    iniciando o app

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './styles';
+import { Title } from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    )
+}
